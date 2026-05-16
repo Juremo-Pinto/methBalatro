3251403ebc32c47abbe229fd117a48d710f3dce8
11-pog <alvessamuel872@gmail.com>
2026-05-16 03:17:52 -0300

    my honest to god attemp

--- meth-balatro/Spritescript.cs
+++ meth-balatro/Spritescript.cs
@@ -4,13 +4,20 @@ using System;
 public partial class Spritescript : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
+	Vector2 mouse;
+	Viewport thing;
+
 	public override void _Ready()
 	{
-		Vector2 cheese = Vector2.Zero;
+		thing = GetViewport();
+		mouse = Vector2.Zero;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		mouse = thing.GetMousePosition();
+		Position = Position.Slerp(mouse, .15f);
+		GD.Print(mouse);
 	}
 }
